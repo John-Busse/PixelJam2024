226c04c1231c586d272f3e64e8f8900fa7217358
John Busse <jbusse@nevada.unr.edu>
2024-05-11 18:45:25 -0700

    started framework for UI & passing variables

--- levels/Stage.gd
+++ levels/Stage.gd
@@ -1,11 +1,14 @@
 extends Node
 
 ### HANDLE ENEMY SPAWNS IN THIS SCRIPT
+export var car_scene: PackedScene
+export var ped_scene: PackedScene
+
 ## Called when the node enters the scene tree for the first time.
-#func _ready():
-#	pass # Replace with function body.
-#
-#
+func _ready():
+	randomize()
+
+
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
--- levels/Tile.gd
+++ levels/Tile.gd
@@ -3,6 +3,10 @@ extends Spatial
 
 signal offscreen #Emit when this tile is offscreen
 
+func _ready():
+	var num_frames: int = $AnimatedSprite3D.frames.get_frame_count("tile")
+	$AnimatedSprite3D.frame = randi() % num_frames
+
 #despawn the tile when it gets offscreen, and emit the signal
 func _on_VisibilityNotifier_screen_exited():
 	emit_signal("offscreen")
